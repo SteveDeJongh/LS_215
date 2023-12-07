2771781cd4e04af29679056ef8cc5883d9c6b9bd
Steve De Jongh <de.jongh.steve@gmail.com>
2023-12-06 16:33:55 -0800

    Lesson 3: small problems

--- small_problems/medium_2/problem_3.js
+++ small_problems/medium_2/problem_3.js
@@ -1,1 +1,54 @@
 // JS210 - Small Problems - Medium 2 - Problem 3 - Tri-Angles
+
+function triangle(x, y, z) {
+  [x,y,z] = [x,y,z].sort();
+  let angles = [x,y,z];
+
+  if (x <= 0 || ((x+y+z) !== 180)) return "invalid";
+  if (angles.some(x => x === 90)) return "right";
+  if (angles.some(x => x > 90)) return 'obtuse';
+  return 'acute';
+}
+
+console.log(triangle(60, 70, 50));       // "acute"
+console.log(triangle(30, 90, 60));       // "right"
+console.log(triangle(120, 50, 10));      // "obtuse"
+console.log(triangle(0, 90, 90));        // "invalid"
+console.log(triangle(50, 50, 50));       // "invalid"
+
+// LS Solution
+
+function triangle(angle1, angle2, angle3) {
+  const angles = [angle1, angle2, angle3];
+  if (isValid(angles)) {
+    return getTriangleType(angles);
+  } else {
+    return "invalid";
+  }
+}
+
+function isValid(angles) {
+  const totalAngle = angles.reduce((total, angle) => total + angle);
+
+  const allNonZero = angles.every(angle => angle > 0);
+
+  return totalAngle === 180 && allNonZero;
+}
+
+function getTriangleType(angles) {
+  if (angles.some(testRightTriangle)) {
+    return "right";
+  } else if (angles.every(testAcuteTriangle)) {
+    return "acute";
+  } else {
+    return "obtuse";
+  }
+}
+
+function testRightTriangle(angle) {
+  return angle === 90;
+}
+
+function testAcuteTriangle(angle) {
+  return angle < 90;
+}
---
+++ small_problems/medium_2/problem_4.js
@@ -0,0 +1,29 @@
+// JS210 - Small Problems - Medium 2 - Problem 4 - Unlucky Days
+
+// Unlucky days occur on Friday the 13th
+  // Can only happen once per month
+
+// Algorithm:
+// create a date object with 13th day, 1st month and `year` year
+// create a `badDays` var
+// Loop from 1 to 12 
+  // set date's month to current iteration number
+  // check the day of date
+  // if it's friday + 1 to badDays
+// return badDays
+
+function fridayThe13ths(year) {
+  let date = new Date(`January 13, ${year}`)
+  let badDays = 0;
+  for (let i = 0; i <= 11; i += 1) {
+    date.setMonth(i);
+    if (date.getDay() === 5) {
+      badDays += 1;
+    }
+  }
+  return badDays;
+}
+
+console.log(fridayThe13ths(1986));      // 1
+console.log(fridayThe13ths(2015));      // 3
+console.log(fridayThe13ths(2017));      // 2
