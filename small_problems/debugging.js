15cc2ec5c8bc3a6753d4f0f855dd05d1b3d4c1a8
Steve De Jongh <de.jongh.steve@gmail.com>
2023-12-10 15:04:14 -0800

    LS 216 prep

---
+++ small_problems/debugging.js
@@ -0,0 +1,165 @@
+// JS 210 - Small Problems - Debugging
+
+/*
+
+// 1) Word Ladder
+
+let ladder = ''; // Was `let ladder = ''`
+
+['head', 'heal', 'teal', 'tell', 'tall', 'tail'].forEach(word => {
+  if (ladder !== '') {
+    ladder += '-'
+  }
+
+  ladder += word
+})
+
+console.log(ladder)  // expect: head-heal-teal-tell-tall-tail
+
+// 2) Reserved Keywords
+
+const RESERVED_KEYWORDS = ['break', 'case', 'catch', 'class', 'const', 'continue',
+  'debugger', 'default', 'delete', 'do', 'else', 'enum', 'export', 'extends', 'finally',
+  'for', 'function', 'if', 'implements', 'import', 'in', 'instanceof', 'interface',
+  'let', 'new', 'package', 'private', 'protected', 'public', 'return', 'static',
+  'super', 'switch', 'this', 'throw', 'try', 'typeof', 'var', 'void', 'while',
+  'with', 'yield'];
+
+function isReserved(name) {
+  let result = false;
+  RESERVED_KEYWORDS.forEach(reserved => {
+    if (name === reserved) {
+      result = true; // Can't return from a forEach loop. Was "return true". Could also change this to a for loop, which can return.
+    }
+  });
+
+  return result;
+}
+
+console.log(isReserved('monkey')); // false
+console.log(isReserved('patch'));  // false
+console.log(isReserved('switch')); // should be: true
+
+Could also just change the method to return the value of `RESERBED_KEYORDS.includes(name)`
+
+// 3 Random Recipe Generator
+
+// Picks n random elements from an array,
+// and returns a new array with those elements.
+function random(array, n) {
+  if (n === undefined) {
+    n = 1;
+  }
+
+  const elements = array.slice();
+  const randomElements = [];
+
+  while (n > 0 && elements.length > 0) {
+    const randomIndex = Math.floor(Math.random() * elements.length);
+    const randomElement = elements[randomIndex];
+
+    randomElements.push(randomElement);
+    elements.splice(randomIndex, 1);
+    n--;
+  }
+
+  return randomElements;
+}
+
+// Ingredients
+
+const ingredients = ['rice', 'green bell pepper', 'mushrooms', 'carrot', 'kebab',
+  'spinach', 'soy bean sprouts', 'mashed potatoes', 'corn', 'cucumber', 'peas'];
+
+const spices = ['peri peri', 'cinnamon', 'nutmeg', 'cardamom', 'ground ginger',
+  'poppy seed', 'cumin'];
+
+const extras = ['peanuts', 'sesame seeds', 'egg', 'wasabi', 'soy sauce'];
+
+// Name
+
+const adjective  = ['Delicious', 'Hot', 'Exotic', 'Creative', 'Festive', 'Dark'];
+const firstNoun  = ['Power', 'After Work', 'Holiday', 'Disco', 'Late Night'];
+const secondNoun = ['Mix', 'Delight', 'Bowl', 'Chunk', 'Surprise', 'Bliss'];
+
+// Generate!
+
+// const dishName = random(adjective) + random(firstNoun) + random(secondNoun); // `+` concatenates the arrays after turnign them into strings. Need to use array.concat() instead.
+const dishName = random(adjective).concat(random(firstNoun)).concat(random(secondNoun));
+// const dish = random(ingredients, 3) + random(spices, 2) + random(extras, 1); // Same thing.
+const dish = random(ingredients, 3).concat(random(spices, 2)).concat(random(extras, 1));
+
+
+console.log(`How about: ${dishName.join(' ')}`);
+console.log(`You need: ${dish.join(', ')}`);
+
+// 4 Task List
+
+const todos = ['wash car', 'exercise', 'buy groceries', 'balance budget',
+             'call plumber', 'feed fido', 'get gas',  'organize closet'];
+
+function addTask(task) {
+  if (todos.includes(task)) {
+    console.log('That task is already on the list.');
+  } else {
+    todos.push(task);
+  }
+}
+
+function completeTasks(n = 1) {
+  let tasksComplete = 0;
+
+  while (todos.length > 0 && tasksComplete < n) {
+    console.log(`${todos[0]} complete!`);
+    // delete todos[0]; // Leaves an empty slot in place of the array element. Need to use shift instead.
+    todos.shift();
+    tasksComplete++;
+  }
+
+  if (todos.length === 0) {
+    console.log('All tasks complete!');
+  } else {
+    console.log(`${tasksComplete} tasks completed; ${todos.length} remaining.`);
+  }
+}
+
+function displayTaskList() {
+  console.log(`ToDo list (${todos.length} tasks):`)
+  console.log('---------------------');
+
+  for (let i = 0; i < todos.length; i++) {
+    console.log(`-- ${todos[i]}`);
+  }
+}
+
+// Utilizing our task manager
+
+addTask('oil change');
+addTask('dentist');
+addTask('homework');
+
+completeTasks(3);
+displayTaskList();
+
+*/
+
+// 5 Range
+
+function range(start, end) {
+  const range = [];
+
+  for (let element = start; element <= end; element++) {
+    range.push(element);
+  }
+
+  return range;
+}
+
+function range(end) {
+  return range(0, end);
+}
+
+// Examples
+
+console.log(range(10, 20));
+console.log(range(5));
