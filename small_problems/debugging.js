eedb8d0eb36ad1f06c546ef1b855f26f1daf4fd9
Steve De Jongh <de.jongh.steve@gmail.com>
2023-12-11 11:14:25 -0800

    LS 216 prep

--- small_problems/debugging.js
+++ small_problems/debugging.js
@@ -219,4 +219,248 @@ addMember('Earl 5mith', '331-9191');
 
 console.log(memberDirectory);
 
+// 7 Molecules
+
+function valence(element) {
+  switch (element) {
+    case 'H': return 1;
+    case 'C': return 4;
+    case 'N': return 5;
+    case 'O': return 6;
+    // omitting the rest
+  }
+}
+
+function valenceOfMolecule(...args) {
+  let sum = 0;
+
+  // arguments.forEach(atom => { // Can explciitely state an arguments argument, and spread values to an array which can be iterated over.
+  args.forEach(atom => {
+  // Array.from(arguments).forEach(atom => { // Alternatively, can turn `arguments` objects into an array.
+    const match   = /([a-zA-Z]+)([0-9]*)/.exec(atom);
+    const element = match[1];
+    const number  = parseInt(match[2]) || 1;
+
+    sum += number * valence(element);
+  });
+
+  return sum;
+}
+
+// Example
+
+console.log('Number of valence electrons');
+console.log('---------------------------');
+console.log(`Water:     ${String(valenceOfMolecule('H2', 'O'))}`);
+console.log(`Propane:   ${String(valenceOfMolecule('C3', 'H8'))}`);
+console.log(`Vitamin C: ${String(valenceOfMolecule('C6', 'H8', 'O6'))}`);
+console.log(`Caffeine:  ${String(valenceOfMolecule('C8', 'H10', 'N4', 'O2'))}`);
+
+// Expected output:
+// Number of valence electrons
+// ---------------------------
+// Water:     8
+// Propane:   20
+// Vitamin C: 68
+// Caffeine:  74
+
+// 8 Glory!
+
+// Standard role-playing dice, ranging from 4 faces to 20,
+// specified in terms of minimum and maximum face value.
+const d4  = {min: 1, max: 4};
+const d6  = {min: 1, max: 6};
+const d8  = {min: 1, max: 8};
+const d10 = {min: 0, max: 9};
+const d12 = {min: 1, max: 12};
+const d20 = {min: 1, max: 20};
+
+function roll({max, min}) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Toss one or more dice and sum up their face values.
+function toss(...args) {
+  const dice = Array.prototype.slice.call(args);
+
+  return dice.map(roll).reduce((sum, value) => sum + value);
+}
+
+// Standard target roll tossing a 20-sided die,
+// with optional bonus and penalty dice.
+// Used to determine whether a character wanting to perform an action
+// succeeds or fails, based on whether the sum of the dice is higher
+// or lower than the relevant character trait.
+// (See below for examples.)
+function targetRoll(characterValue, bonus = {min: 0, max: 0}, penalty = {min: 0, max: 0}) {
+  let result = toss(d20, bonus, penalty);
+  // Normalize in case bonus or penalty push result out of the D20 range.
+  result = Math.max(1, result);
+  result = Math.min(20, result);
+
+  console.log(`--> ${result}`);
+
+  switch (result) {
+    case 1:
+      automaticFail();
+      break; // Needed to add break statements to avoid "falling through"
+    case 20:
+      automaticSuccess();
+      break;
+    default: result >= characterValue ? success() : fail();
+  }
+}
+
+function success() {
+  console.log('Your character succeeds.');
+}
+
+function fail() {
+  console.log('Your character fails.');
+}
+
+function automaticSuccess() {
+  console.log('Your character succeeds without effort. Glory!');
+}
+
+function automaticFail() {
+  console.log('Meagre attempt. Your character fails miserably.');
+}
+
+// Example character.
+const myCharacter = {
+  name: 'Jenkins',
+  strength: 4,
+  constitution: 6,
+  education: 11,
+  luck: 3,
+  sanity: 9,
+};
+
+// Example rolls:
+
+// Jenkins wants to break in a door with brute force,
+// so he has to roll against his strength value.
+targetRoll(myCharacter.strength);
+
+// Jenkins is challenged to a drinking contest.
+// In order to determine how much he can take, he rolls against his
+// constitution. Since he just ate a huge portion of pork roast, he
+// gets a D4 bonus die.
+targetRoll(myCharacter.constitution, {min: 0, max: 4});
+
+// Jenkins found an ancient scroll and attempts to decipher it.
+// He has to roll against his education, in order to determine
+// whether he's able to read it.
+targetRoll(myCharacter.education);
+
+// 9 Grade Analysis
+
+function average(nums) {
+  const sum = nums.reduce((total, num) => total + num);
+
+  return sum / nums.length;
+}
+
+function median(nums) {
+  nums.sort((a, b) => a - b); // Need to explcitely tell sort to sort the number values as numbers, and not coerce into strings and compare character by character.
+
+  let median;
+  const length = nums.length;
+  if (length % 2 === 0) {
+    median = average([nums[(length / 2) - 1], nums[length / 2]]);
+  } else {
+    median = nums[Math.floor(length / 2)];
+  }
+
+  return median;
+}
+
+// Tests
+
+const quarter1ExamScores = [89, 72, 100, 93, 64, 97, 82, 87, 90, 94];
+const quarter2ExamScores = [76, 91, 89, 90, 91, 67, 99, 82, 91, 87];
+const quarter3ExamScores = [99, 91, 88, 72, 76, 64, 94, 79, 86, 88];
+const quarter4ExamScores = [100, 94, 73, 88, 82, 91, 97, 99, 80, 84];
+
+// should all log 'true':
+console.log(average(quarter1ExamScores) === 86.8);
+console.log(average(quarter2ExamScores) === 86.3);
+console.log(average(quarter3ExamScores) === 83.7);
+console.log(average(quarter4ExamScores) === 88.8);
+
+console.log(median(quarter1ExamScores) === 89.5);
+console.log(median(quarter2ExamScores) === 89.5);
+console.log(median(quarter3ExamScores) === 87);
+console.log(median(quarter4ExamScores) === 89.5);
+
 */
+
+// 10 Weekday Classes
+
+const TODAY = toDate("2018-08-05");
+
+function toDate(string) {
+  return new Date(`${string}T00:00:00`);
+}
+
+function toString(date) {
+  return `${date.getYear()}-${date.getMonth()}-${date.getDay()}`;
+}
+
+function isInThePast(date) {
+  return date < TODAY;
+}
+
+function isWeekday(date) {
+  return date.getDay() >= 1 &&
+         date.getDay() <= 5;
+}
+
+const myCalendar = {
+  "2018-08-13": ["JS debugging exercises"],
+  "2018-08-14": ["Read 'Demystifying Rails'", "Settle health insurance"],
+  "2018-08-15": ["Read 'Demystifying Rails'"],
+  "2018-08-16": [],
+  "2018-08-30": ["Drone video project plan"],
+  "2018-09-10": ["Annual servicing of race bike"],
+  "2018-09-12": ["Study"],
+  "2018-11-02": ["Birthday Party"],
+  "2018-11-03": ["Birthday Party"],
+};
+
+const offeredClasses = {
+  "Back To The Future Movie Night": ["2018-07-30"],
+  "Web Security Fundamentals": ["2018-09-10", "2018-09-11"],
+  "Pranayama Yoga For Beginners": ["2018-08-30", "2018-08-31", "2018-09-01"],
+  "Mike's Hikes": ["2018-08-16"],
+  "Gordon Ramsay Master Class": ["2018-09-11", "2018-09-12"],
+  "Powerboating 101": ["2018-09-15", "2018-09-16"],
+  "Discover Parachuting": ["2018-11-02"],
+};
+
+function getCompatibleEvents(classes, calendar) {
+  function isAvailable(date) {
+    const dateStr = toString(date);
+    return !calendar[dateStr] || calendar[dateStr].length === 0;
+  };
+
+  const compatibleClasses = [];
+
+  Object.keys(classes).forEach(className => {
+    const classDates = classes[className].map(toDate);
+
+    if (classDates.some(isInThePast)) {
+      return;
+    }
+
+    if (classDates.filter(isWeekday).every(isAvailable)) {
+      compatibleClasses.push(className);
+    }
+  });
+
+  return compatibleClasses;
+}
+
+console.log(getCompatibleEvents(offeredClasses, myCalendar));
+// expected: ["Mike's Hikes", "Powerboating 101"]
